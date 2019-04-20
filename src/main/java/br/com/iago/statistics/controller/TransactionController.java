937a70c2bd0f7968a8c687d23af70fdd1baf701a
Iago <iagofontes@hotmail.com>
2019-04-20 12:09:11 -0300

    Implementação de erros ok, verificar instruções para inicialização.

--- src/main/java/br/com/iago/statistics/controller/TransactionController.java
+++ src/main/java/br/com/iago/statistics/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package br.com.iago.statistics.controller;
 
+import br.com.iago.statistics.exception.OldTransactionException;
 import br.com.iago.statistics.model.Transaction;
 import br.com.iago.statistics.service.TransactionService;
 import io.swagger.annotations.Api;
@@ -10,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -27,9 +26,9 @@ public class TransactionController {
 
     @ApiOperation(value = "Realiza a validação e persistência da transação informada no corpo da requisição")
     @PostMapping("/transaction")
-    public ResponseEntity<Void> createTransaction(@Valid @NotNull @RequestBody Transaction transaction){
+    public ResponseEntity<Void> createTransaction(@Valid @NotNull @RequestBody Transaction transaction) throws OldTransactionException {
         if((System.currentTimeMillis() - transaction.getTimestamp()) > SECONDS_LIMIT) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            throw new OldTransactionException("A transação ocorreu a mais de 60 segundos");
         } else {
             transactionService.saveTransaction(transaction);
             return new ResponseEntity<>(HttpStatus.CREATED);
