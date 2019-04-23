251ccf58ae0b1cff73114c29e877cecdd13da177
Iago <iagofontes@hotmail.com>
2019-04-22 21:58:04 -0300

    final commit

--- src/main/java/br/com/iago/statistics/service/StatisticsService.java
+++ src/main/java/br/com/iago/statistics/service/StatisticsService.java
@@ -14,7 +14,7 @@ import java.util.concurrent.PriorityBlockingQueue;
 @Service
 public class StatisticsService {
 
-    private static final int INTERVALO_ATUALIZACAO_MILISSEG =  100000;
+    private static final int INTERVALO_ATUALIZACAO_MILISSEG =  10000;
     private static final int CAPACIDADE_LISTA =  100;
 
     private final PriorityBlockingQueue<Transaction> ultimasTransacoes =
